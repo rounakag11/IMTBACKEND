278c57b9432e45401aea00b786f004dbe98ae0c2
rounakagrawal11 <rounak.agrawal@capgemini.com>
2023-07-11 10:43:41 +0530

    add firebase setup for node with new confrugution

--- config.js
+++ config.js
@@ -1,17 +1,11 @@
-const {initializeApp} = require('firebase/app')
-const  {getFirestore}  =  require('firebase/firestore/lite')
+var admin = require("firebase-admin");
 
-const firebaseConfig = {
-  apiKey: "AIzaSyC-0SwCY8WeszQE_VR31TGSyTaWCvwjOsk",
-  authDomain: "inventory-management-tools.firebaseapp.com",
-  projectId: "inventory-management-tools",
-  storageBucket: "inventory-management-tools.appspot.com",
-  messagingSenderId: "402602190337",
-  appId: "1:402602190337:web:a75405d45260eec6794200",
-  measurementId: "G-56NZR7Z6V6",
-};
-const app = initializeApp(firebaseConfig);
-const database = getFirestore(app);
-const collectionName = database.collection("InventoryDB")
+var serviceAccount = require("path/to/serviceAccountKey.json");
 
-module.exports= collectionName
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount)
+});
+
+const database = admin.firestore()
+
+module.exports = database
--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const collectionName = require('./config');
+const database = require('./config');
 
 
 const app = express();
@@ -7,24 +7,26 @@ app.use(express.json());
 
 // Signup endpoint
 app.post('/signup', (req, res) => {
-  const { email, password } = req.body;
 
-  collectionName
-    .auth()
-    .createUser({
-      email: email,
-      password: password,
-    })
-    .then((userRecord) => {
-      // User created successfully
-      console.log('Successfully created new user:', userRecord.uid);
-      res.status(200).json({ message: 'User created successfully' });
-    })
-    .catch((error) => {
-      // Error occurred during user creation
-      console.log('Error creating new user:', error);
-      res.status(500).json({ error: 'Failed to create user' });
-    });
+  const data = req.body
+  console.log('data',data)
+  // const { email, password } = req.body;
+  // const docRef = database.collection("Users").doc("kKmLrXj6pA3jvAUwB4BQ")
+
+    // docRef.set({
+    //   username : email,
+    //   password : password
+    // })
+    // .then((userRecord) => {
+    //   // User created successfully
+    //   console.log('Successfully created new user:', userRecord.uid);
+    //   res.status(200).json({ message: 'User created successfully' });
+    // })
+    // .catch((error) => {
+    //   // Error occurred during user creation
+    //   console.log('Error creating new user:', error);
+    //   res.status(500).json({ error: 'Failed to create user' });
+    // });
 });
 
 // Start the server
