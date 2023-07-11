21c39807c4eb54a3a26d16e50bb8af7df4a3dba0
rounakagrawal11 <rounak.agrawal@capgemini.com>
2023-07-11 22:47:28 +0530

    new commit for repo

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 var admin = require("firebase-admin");
 
-var serviceAccount = require("path/to/serviceAccountKey.json");
+var serviceAccount = require("./inventory-management-t-firebase-adminsdk-fb3uu-54a2b65d9e.json");
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount)
--- index.js
+++ index.js
@@ -6,27 +6,27 @@ const app = express();
 app.use(express.json());
 
 // Signup endpoint
-app.post('/signup', (req, res) => {
+app.post('/signup', async(req, res) => {
+  const { email, password } = req.body;
+  const snapshot = await database.collection('users').get();
+  console.log('snapshot',snapshot)
 
-  const data = req.body
-  console.log('data',data)
-  // const { email, password } = req.body;
-  // const docRef = database.collection("Users").doc("kKmLrXj6pA3jvAUwB4BQ")
 
-    // docRef.set({
-    //   username : email,
-    //   password : password
-    // })
-    // .then((userRecord) => {
-    //   // User created successfully
-    //   console.log('Successfully created new user:', userRecord.uid);
-    //   res.status(200).json({ message: 'User created successfully' });
-    // })
-    // .catch((error) => {
-    //   // Error occurred during user creation
-    //   console.log('Error creating new user:', error);
-    //   res.status(500).json({ error: 'Failed to create user' });
-    // });
+  const docRef = database.collection("InventoryDb").doc(`${email}`)
+   await docRef.set({
+      email_id : email,
+      password : password
+    })
+    .then((userRecord) => {
+      // User created successfully
+      console.log('Successfully created new user:', userRecord);
+      res.status(200).json({ message: 'User created successfully' });
+    })
+    .catch((error) => {
+      // Error occurred during user creation
+      console.log('Error creating new user:', error);
+      res.status(500).json({ error: 'Failed to create user' });
+    });
 });
 
 // Start the server
