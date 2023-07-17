19458d763083bab6756e41454d190cb0ceecd346
rounakag11 <rounakagrawal1997@gmail.com>
2023-07-17 11:52:23 +0530

    commitaddforgetPassword

--- forgetpassword.js
+++ forgetpassword.js
@@ -1,29 +1,23 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-const database = require('./config'); 
+const express = require("express");
+const bodyParser = require("body-parser");
+const database = require("./config");
 
 const app = express();
 app.use(bodyParser.json());
 
-
-app.post('/forgetPassword', async (req, res) => {
-  const { CG_Email_Address, password } = req.body;
-  
-  try {
-    const docRef = database.collection("InventoryDb").doc(email);
-    const docSnapshot = await docRef.get();
-
-    if (docSnapshot.exists) {
-      
-      await docRef.update({ password });
-
-      res.status(200).json({ message: 'Password updated successfully' });
-    } else {
-      res.status(404).json({ error: 'User not found' });
-    }
-  } catch (error) {
-    console.log('Error updating password:', error);
-    res.status(500).json({ error: 'Failed to update password' });
+app.post("/forgetPassword", async (req, res) => {
+  const { CG_ID } = req.body;
+  const docRef = database.collection("CG_SignUp_DB").doc(`${CG_ID}`);
+  const docSnapshot = await docRef.get();
+  if (docSnapshot.exists) {
+    res
+      .status(200)
+      .json({
+        message: "User Exist",
+        verficationOtp: ("" + Math.random()).substring(2, 6)
+      });
+  } else {
+    res.status(404).json({ error: "User not exsist please do register first" });
   }
 });
 
